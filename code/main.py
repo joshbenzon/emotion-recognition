88ec8e63c5080379b3f14c3b66e3d20f4bee7e85
Theo Fernandez <theofernandez@theos-mbp.devices.brown.edu>
2021-12-06 16:06:55 -0500

    Changed main.py to fix some bugs

--- code/main.py
+++ code/main.py
@@ -4,9 +4,10 @@ import os
 from model import Model
 from preprocess import Datasets
 import hyperparameters as hp
+import tensorflow as tf
 
 
-def train(model, datasets, checkpoint_path, logs_path, init_epoch):
+def train(model, datasets, init_epoch):
     model.fit(
         x=datasets.train_data,
         validation_data=datasets.test_data,
@@ -26,25 +27,25 @@ def test(model, test_data):
 def main():
     print("Starting CNN for Inside Out Group")
     time = datetime.now()
-    timestamp = time_now.strftime("%m%d%y-%H%M%S")
     init_epoch = 0
 
     relative_path = ".." + os.sep + "data" + os.sep
     absolute_path = os.path.abspath(relative_path)
 
-    os.chdir(sys.path[0])
-
-    dataset = Datasets(absolute_path)
+    datasets = Datasets(absolute_path)
 
     model = Model()
     model(tf.keras.Input(shape=(hp.img_size, hp.img_size, 3)))
-    checkpoint_path = "checkpoints" + os.sep + \
-        "model" + os.sep + timestamp + os.sep
-    logs_path = "logs" + os.sep + "model" + \
-        os.sep + timestamp + os.sep
 
     # Print summary of model
     model.summary()
+
+    model.compile(
+        optimizer=model.optimizer,
+        loss=model.loss_fn,
+        metrics=["sparse_categorical_accuracy"])
+
+    train(model, datasets, init_epoch)
 
 
 if __name__ == '__main__':
