e1147b771b24ecdc95bc56d5c20e3b14e69e1f24
Theo Fernandez <theofernandez@theos-mbp.devices.brown.edu>
2021-12-12 17:28:14 -0500

    Modified Preprocessing

--- code/preprocess.py
+++ code/preprocess.py
@@ -186,9 +186,9 @@ class Datasets():
 
             data_gen = tf.keras.preprocessing.image.ImageDataGenerator(
                 preprocessing_function=self.preprocess_fn,
-                rotation_range=20,
-                width_shift_range=0.2,
-                height_shift_range=0.2,
+                rotation_range=30,
+                width_shift_range=0.25,
+                height_shift_range=0.25,
                 horizontal_flip=True)
 
             # ============================================================
