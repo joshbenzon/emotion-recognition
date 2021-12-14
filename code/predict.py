92dd734965ae8057824ff703cecfa3ec7a2daf10
Katie Baumgarten <catherine_baumgarten@brown.edu>
2021-12-13 21:04:53 -0500

    hm

--- code/predict.py
+++ code/predict.py
@@ -5,12 +5,11 @@ import hyperparameters as hp
 
 def predict_image(path):
     model = VGGModel
-    img = tf.keras.preprocessing.image.load_img(path, color_mode="grayscale", target_size=(48, 48))
+    img = tf.keras.preprocessing.image.load_img(path, color_mode="grayscale", grayscale = True, target_size=(48, 48))
     print("loaded image in yay")
 
     label_dict = {0:'Angry',1:'Disgust',2:'Fear',3:'Happy',4:'Neutral',5:'Sad',6:'Surprise'}
     img = np.array(img)
-    img.shape[3] = 1
     print(img.shape)
 
     img = np.expand_dims(img, axis=0)
