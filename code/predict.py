8f922dfa749ac1c5d511a7296f19c759cb1a63d8
Josh Benzon <benzonjosh@gmail.com>
2021-12-14 16:11:08 -0500

    laod weights for predict

--- code/predict.py
+++ code/predict.py
@@ -6,38 +6,41 @@ from tensorflow.keras.layers import \
     Conv2D, MaxPool2D, Dropout, Flatten, Dense, ZeroPadding2D, BatchNormalization
 
 
-# def create_model():
-#     model = tf.keras.Sequential()
-#     # model.build(input_shape)
-#
-#     model.add(Conv2D(64, 3, 1, padding="same", activation="relu", name="block1_conv1"))
-#     model.add(Conv2D(64, 3, 1, padding="same", activation="relu", name="block1_conv2"))
-#     model.add(MaxPool2D(2, name="block1_pool"))
-#     model.add(Conv2D(128, 3, 1, padding="same", activation="relu", name="block2_conv1"))
-#     model.add(Conv2D(128, 3, 1, padding="same", activation="relu", name="block2_conv2"))
-#     model.add(MaxPool2D(2, name="block2_pool"))
-#     model.add(Conv2D(256, 3, 1, padding="same", activation="relu", name="block3_conv1"))
-#     model.add(Conv2D(256, 3, 1, padding="same", activation="relu", name="block3_conv2"))
-#     model.add(Conv2D(256, 3, 1, padding="same", activation="relu", name="block3_conv3"))
-#     model.add(MaxPool2D(2, name="block3_pool"))
-#     model.add(Conv2D(512, 3, 1, padding="same", activation="relu", name="block4_conv1"))
-#     model.add(Conv2D(512, 3, 1, padding="same", activation="relu", name="block4_conv2"))
-#     model.add(Conv2D(512, 3, 1, padding="same", activation="relu", name="block4_conv3"))
-#     model.add(MaxPool2D(2, name="block4_pool"))
-#     model.add(Conv2D(512, 3, 1, padding="same", activation="relu", name="block5_conv1"))
-#     model.add(Conv2D(512, 3, 1, padding="same", activation="relu", name="block5_conv2"))
-#     model.add(Conv2D(512, 3, 1, padding="same", activation="relu", name="block5_conv3"))
-#     model.add(MaxPool2D(2, name="block5_pool"))
-#
-#     model.save('new_model')
+def create_model():
+    model = tf.keras.Sequential()
+    # model.build(input_shape)
+
+    model.add(Conv2D(64, 3, 1, padding="same", activation="relu", name="block1_conv1"))
+    model.add(Conv2D(64, 3, 1, padding="same", activation="relu", name="block1_conv2"))
+    model.add(MaxPool2D(2, name="block1_pool"))
+    model.add(Conv2D(128, 3, 1, padding="same", activation="relu", name="block2_conv1"))
+    model.add(Conv2D(128, 3, 1, padding="same", activation="relu", name="block2_conv2"))
+    model.add(MaxPool2D(2, name="block2_pool"))
+    model.add(Conv2D(256, 3, 1, padding="same", activation="relu", name="block3_conv1"))
+    model.add(Conv2D(256, 3, 1, padding="same", activation="relu", name="block3_conv2"))
+    model.add(Conv2D(256, 3, 1, padding="same", activation="relu", name="block3_conv3"))
+    model.add(MaxPool2D(2, name="block3_pool"))
+    model.add(Conv2D(512, 3, 1, padding="same", activation="relu", name="block4_conv1"))
+    model.add(Conv2D(512, 3, 1, padding="same", activation="relu", name="block4_conv2"))
+    model.add(Conv2D(512, 3, 1, padding="same", activation="relu", name="block4_conv3"))
+    model.add(MaxPool2D(2, name="block4_pool"))
+    model.add(Conv2D(512, 3, 1, padding="same", activation="relu", name="block5_conv1"))
+    model.add(Conv2D(512, 3, 1, padding="same", activation="relu", name="block5_conv2"))
+    model.add(Conv2D(512, 3, 1, padding="same", activation="relu", name="block5_conv3"))
+    model.add(MaxPool2D(2, name="block5_pool"))
+    model.add(Dropout(rate=0.3))
+    model.add(Flatten())
+    mdoel.add(Dense(units=128, activation='relu'))
+    model.add(Dense(units=7, activation='softmax'))
+
+    model.save('new_model')
 
 
 def predict_image(path):  # removed model
-    model = VGGModel()
-    # model = tf.keras.models.load_model('new_model')
-    
-    # model.load_weights("vgg.weights.e024-acc0.4742.h5")
-    # model.load_weights("vgg.weights.e024-acc0.4742.h5")
+    # model = VGGModel()
+    model = tf.keras.models.load_model('new_model')
+
+    model.load_weights("vgg.weights.e024-acc0.4742.h5")
 
     img = tf.keras.preprocessing.image.load_img(path, color_mode="grayscale", grayscale=True, target_size=(48, 48))
 
