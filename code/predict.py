50483aef918b5093c58638bbad112f94483a8166
Katie Baumgarten <catherine_baumgarten@brown.edu>
2021-12-13 22:35:09 -0500

    load

--- code/predict.py
+++ code/predict.py
@@ -4,7 +4,8 @@ from models import YourModel, VGGModel
 import hyperparameters as hp
 
 def predict_image(path):
-    model = tf.keras.models.load_model("\121321-205349\vgg.weights.e024-acc0.4742.h5")
+    model = VGGModel()
+    model.load_weights("\121321-205349\vgg.weights.e024-acc0.4742.h5")
     # model.save_weights("\121321-205349\vgg.weights.e024-acc0.4742.h5")
     # print(model)
 
