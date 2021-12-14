d92fd033ce4fa16f40bd5ea788f52528ebe0f206
Josh Benzon <benzonjosh@gmail.com>
2021-12-14 16:19:35 -0500

    laod weights for predict

--- code/predict.py
+++ code/predict.py
@@ -8,7 +8,7 @@ from tensorflow.keras.layers import \
 
 def create_model():
     model = tf.keras.Sequential()
-    tf.keras.models.Model.build(input_shape=(48, 48, 1))
+    model.tf.keras.models.Model.build((48, 48, 1))
 
     model.add(Conv2D(64, 3, 1, padding="same", activation="relu", name="block1_conv1"))
     model.add(Conv2D(64, 3, 1, padding="same", activation="relu", name="block1_conv2"))
