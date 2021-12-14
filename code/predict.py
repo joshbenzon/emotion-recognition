17e8a5a568d97b923910c38454b99d2f574fad2e
Theo Fernandez <theofernandez@theos-mbp.devices.brown.edu>
2021-12-14 17:57:31 -0500

    Debugging

--- code/predict.py
+++ code/predict.py
@@ -12,31 +12,36 @@ abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
 
+print("Gettings Model...")
 model = VGGModel()
 
-print("Loading Models...")
+print("Assigning Model Shape...")
+model(tf.keras.Input(shape=(224, 224, 3)))
+
+# print("Loading Models...")
 # model.vgg16 = load_model("vgg.h5")
-model.head = load_model("head.h5")
+# model.head = load_model("head.h5")
 
-# print("Loading Weights...")
-# model.vgg16.load_weights("vgg16_imagenet.h5", by_name=True)
-# model.head.load_weights(
-#     "checkpoints/vgg_model/121421-151746/vgg.weights.e018-acc0.5242.h5")
+print("Loading Weights...")
+model.vgg16.load_weights("vgg16_imagenet.h5", by_name=True)
+model.head.load_weights(
+    "checkpoints/vgg_model/121421-151746/vgg.weights.e018-acc0.5242.h5")
 
-# model.compile(
-#     optimizer=model.optimizer,
-#     loss=model.loss_fn,
-#     metrics=["sparse_categorical_accuracy"])
+print("Compiling Model...")
+model.compile(
+    optimizer=model.optimizer,
+    loss=model.loss_fn,
+    metrics=["sparse_categorical_accuracy"])
 
 
-# image_path = "angry.png"
+image_path = "angry.png"
 
-# print("Loading in Image...")
-# image = tf.keras.preprocessing.image.load_img(image_path)
-# input_arr = tf.keras.preprocessing.image.img_to_array(image)
-# input_arr = np.array([input_arr])  # Convert single image to a batch.
+print("Loading in Image...")
+image = tf.keras.preprocessing.image.load_img(image_path)
+input_arr = tf.keras.preprocessing.image.img_to_array(image)
+input_arr = np.array([input_arr])  # Convert single image to a batch.
 
-# print("Predicting Image...")
-# predictions = model.predict(input_arr)
+print("Predicting Image...")
+predictions = model.predict(input_arr)
 
-# print("Finished!")
+print("Finished!")
