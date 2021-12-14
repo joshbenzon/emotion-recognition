120d11d8334074b457a03a68ed7f439360305639
Theo Fernandez <theofernandez@Theos-MacBook-Pro.local>
2021-12-14 14:43:39 -0500

    Added Minimally Functional GUI

--- code/camera.py
+++ code/camera.py
@@ -5,17 +5,17 @@ import hyperparameters as hp
 import numpy as np
 from skimage.transform import rescale, resize
 
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
 
 def takePicture():
-    abspath = os.path.abspath(__file__)
-    dname = os.path.dirname(abspath)
-    os.chdir(dname)
-
     cap = cv2.VideoCapture(0)
 
     while True:
         _, frame = cap.read()
-        cv2.imshow("Frame", frame)
+        cv2.imshow("Take A Photo with an Emotion", frame)
 
         key = cv2.waitKey(1)
         if key == 27:
@@ -27,16 +27,45 @@ def takePicture():
     cap.release()
     cv2.destroyAllWindows()
 
-    # img = Image.open("image.jpg").convert("L")
 
-    # img = resize(img, (hp.img_size, hp.img_size))
+def findFace():
+    # Get user supplied values
+    imagePath = "image.jpg"
+    cascPath = "haarcascade_frontalface_default.xml"
 
-    # img.thumbnail((48, 48), Image.ANTIALIAS)
+    # Create the haar cascade
+    faceCascade = cv2.CascadeClassifier(cascPath)
 
-    # img = np.array(img)
-    # img = resize(img, (48, 48))
-    # img = Image.fromarray(img)
-    # img.save("image.jpg")
+    # Read the image
+    image = cv2.imread(imagePath)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the image
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(200, 200),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
+    print("Found {0} faces!".format(len(faces)))
+
+    x, y, w, h = faces[0]
+
+    formatImage(x, y, w, h)
 
 
-takePicture()
+def formatImage(x, y, w, h):
+    image = Image.open("image.jpg").convert("L")
+    image = image.crop((x, y, x+w, y+h))
+    image.thumbnail((48, 48), Image.ANTIALIAS)
+    image.save('processed_image.jpg', quality=95)
+
+
+def detectEmotion():
+    takePicture()
+    findFace()
+    # predict emotion using tf, return emotion
+    emotion = "Neutral"
+    return emotion
--- code/gui.py
+++ code/gui.py
@@ -1,24 +1,44 @@
-from tkinter import Tk, Label, Button
+from camera import detectEmotion
+from tkinter import *
+from PIL import ImageTk, Image
+import os
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
 
 
-class MyFirstGUI:
+class GUI:
     def __init__(self, master):
         self.master = master
-        master.title("A simple GUI")
+        master.title("Inside Out Emotion Recognition")
 
-        self.label = Label(master, text="This is our first GUI!")
+        self.label = Label(master, text="Take a Picture of Your Face!")
         self.label.pack()
 
-        self.greet_button = Button(master, text="Greet", command=self.greet)
-        self.greet_button.pack()
+        self.camera_button = Button(
+            master, text="Take Picture", command=self.getEmotion)
+        self.camera_button.pack()
 
-        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button = Button(master, text="Quit", command=master.quit)
         self.close_button.pack()
 
-    def greet(self):
-        print("Greetings!")
+        self.output = Text(root, height=1,
+                           width=13,
+                           bg="light blue")
+        self.output.pack()
 
+    def getEmotion(self):
+        self.output.delete("1.0", END)
+        emotion = detectEmotion()
+        self.output.insert(END, emotion)
+
+
+if os.path.exists("image.jpg"):
+    os.remove("image.jpg")
+    os.remove("processed_image.jpg")
 
 root = Tk()
-my_gui = MyFirstGUI(root)
+
+gui = GUI(root)
 root.mainloop()
